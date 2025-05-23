9c05d599c369f8eb1df8035e604c2c9d6a4ee9f5
adarsh <adarshchaturvedi0207@gmail.com>
2025-05-24 00:24:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function cursorAnimation() {
+//   // Cursor making
+//   document.addEventListener("mousemove", function (dets) {
+//     // console.log("hello world");
+//     console.log(dets.x);
+//     gsap.to("#cursor", {
+//       left: dets.x,
+//       top: dets.y,
+//     });
+//   });
+//   // magnet maker
+//   //   Shery.makeMagnet("#nav-part2 h4");
+// }
+// cursorAnimation();
